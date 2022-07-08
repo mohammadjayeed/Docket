a0ec2e52661c8c2fbd6d2dd57afa12895c391c37
Mohammad Jayeed <thisisjayeed@gmail.com>
2022-07-09 02:45:20 +0600

    completed project - updated v2

--- docket/wsgi.py
+++ docket/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for taskmate project.
+WSGI config for docket project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docket.settings')
 
 application = get_wsgi_application()
