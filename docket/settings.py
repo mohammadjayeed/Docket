40927edb9b6e124c1be6cad235177524a25da5a7
Mohammad Jayeed <thisisjayeed@gmail.com>
2022-07-12 11:10:37 +0600

    updated v3

--- docket/settings.py
+++ docket/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$o*_h^t0+_bosd6q_i%-$%9gln*@(p409o^k29#y&27@_^d$70'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
